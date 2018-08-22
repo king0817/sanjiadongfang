3bb1cf1ec9aa88bc973b95e14ab64252f48e3aa8
王萌 <17778027023@qq.com>
2018-08-22 17:26:46 +0800

    项目上传

---
+++ js/sanjia.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container', {
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+$(function () {
+
+});
